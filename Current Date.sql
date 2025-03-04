7c178c864340487bfe18b1c3ac60f54dd321d51a
Someshwar K <warsomesh22@gmail.com>
2025-03-04 20:52:33 +0530

    Queries to get the Date from the System
    Queries to identify Different dates from System.

---
+++ Current Date.sql
@@ -0,0 +1,4 @@
+select curdate()+interval 2 week; -- After 2 weeks from today's date
+select curdate()+interval 12 day; -- After 12 days from today's date
+select curdate()+interval 2 month; -- After 2 months from today's date
+select curdate()+interval 4 year; -- After 4 years from today's date
